740fff18de6c250f3ce0848116fd9e8469c80572
Harsh <pandeygautam039@gmail.com>
2026-05-07 07:01:44 +0530

    update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "./components/Sidebar";
+import ChatArea from "./components/ChatArea";
 
 function App() {
   return (
@@ -7,9 +8,7 @@ function App() {
         <Sidebar />
 
         <main className="flex-1 flex flex-col relative border-l border-black">
-          <div className="flex items-center justify-center h-full text-cyan-400">
-            [ Main Chat Area Pending ]
-          </div>
+          <ChatArea />
         </main>
       </div>
     </div>
